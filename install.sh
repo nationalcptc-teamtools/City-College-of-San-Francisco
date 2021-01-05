0643e453be61ad637ec8f5cee250a6e899fd4266
Zoey G <code@zoeyinthe.cloud>
2021-01-05 09:26:34 +0000

    Fix git repo url in install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-git clone https://github.com/zoeyg/cptc.git ~/tools
+git clone https://github.com/nationalcptc-teamtools/City-College-of-San-Francisco.git ~/tools
 chmod +x ~/tools/setup-kali.sh
 sudo ~/tools/setup-kali.sh $(whoami)
